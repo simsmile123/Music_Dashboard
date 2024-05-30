3752e7012c9db19353fee95ca699034d2b5e089a
Vento24 <mvento@kountable.com>
2024-05-30 13:53:18 -0400

    firebase troubleshooting

--- frontend/src/routes/Chat.jsx
+++ frontend/src/routes/Chat.jsx
@@ -1,7 +1,8 @@
 import '../styles/chat.css'
 import Message from '../components/Message';
 import MessageSend from '../components/MessageSend';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 const Chat = () => {
     const [isMe, setIsMe] = useState(true)
@@ -10,25 +11,38 @@ const Chat = () => {
         console.log("message to be send: ", message)
     }
 
+    const fetchMessages = async () => {
+        try {
+            const response = await axios.get('http://localhost:5001/messages')
+            console.log(response, " -> API Response")
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    useEffect(() => {
+        fetchMessages()
+    }, [])
+
     return (
         <div className='main-container'>  
-        <div className="navbar">  </div>
-        <div className='main-content'>
-            <div className='chat-title-container'> 
-                <h1 className='main-title'> Cariah</h1>
-            </div>
-            <h2> Inbox</h2>
-            <div className="chat-container">
-                <Message me={false}/>
-                <Message me={true}/>
-                <Message me={false}/>
-                <Message me={false}/>
-
-                <div className='send-container'>
-                    <MessageSend sendMessage={sendMessage} />
+            <div className="navbar">  </div>
+            <div className='main-content'>
+                <div className='chat-title-container'> 
+                    <h1 className='main-title'> Cariah</h1>
                 </div>
-            </div>
-        </div>    
+                <h2> Inbox</h2>
+                <div className="chat-container">
+                    <Message me={false}/>
+                    <Message me={true}/>
+                    <Message me={false}/>
+                    <Message me={false}/>
+
+                    <div className='send-container'>
+                        <MessageSend sendMessage={sendMessage} />
+                    </div>
+                </div>
+            </div>    
         </div>
     )
 };
