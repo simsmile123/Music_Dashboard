c2dc01ff67df5de78801401192f6f761b197532b
Vivi Thai <vithai1@berkeley.edu>
2024-05-30 08:22:12 -0700

    basic inbox interface

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ import TopSongs from './routes/TopSongs.jsx';
 import Explore from './routes/Explore.jsx';
 import Inbox from './routes/Inbox.jsx';
 import Forum from './routes/Forum.jsx';
-import Chat from './routes/components/Chat.jsx';
+import Chat from './components/Chat.jsx';
 import './index.css'
 
 const router = createBrowserRouter([
